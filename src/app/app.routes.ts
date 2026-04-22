c49e952afbe338f9c2cb4ce7592fe1c95fcbc343
umavanmoriel <umeda19971102@gmail.com>
2026-04-22 23:27:01 +0200

    --Skapar komponenter för header och footer --Lägge till navigation i header --Lägger till css för header och footer komponenter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Startpage } from './startpage/startpage';
 import { About } from './about/about';
 import { Converter } from './converter/converter'
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
     { path: 'startpage', component : Startpage },
     { path: 'about', component : About },
-    { path: 'converter', component : Converter }
+    { path: 'converter', component : Converter },
+    { path: '', redirectTo: '/startpage', pathMatch: 'full'},
+    { path: 'not-found', component : NotFound },
+    { path: '**', component : NotFound}
 ];
