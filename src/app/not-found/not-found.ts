24815f4f7227b291c38de0aec4e299a59b3deb6e
umavanmoriel <umeda19971102@gmail.com>
2026-04-23 12:48:47 +0200

    --Skapar image mapp i public för att lägga till bilder --Lägger till css för not-found komponent -- Lägger till class NotFound för bild och knapp

--- src/app/not-found/not-found.ts
+++ src/app/not-found/not-found.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
-export class NotFound {}
+export class NotFound {
+
+  errorImage : string = "/images/error-page-svgrepo-com.svg";
+
+  constructor(private router: Router) {}
+
+  returnToStartpage() : void {
+    this.router.navigate(['/startpage']);
+  }
+}
