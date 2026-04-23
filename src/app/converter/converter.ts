a3ecd439b3e07ab5948a09450dc370a5769e890f
umavanmoriel <umeda19971102@gmail.com>
2026-04-23 18:18:23 +0200

    --Skapar input för meter/feet konverter i converter.html
    --Lägger till metoder class Converter för att konvertera meter till feet och vice versa i converter.ts
    
    --Ändrar css för konverter

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -11,6 +11,8 @@ export class Converter {
   // Properties
   celsiusValue : number = 0;
   fahrenValue : number = 32;
+  meterValue : number = 0;
+  feetValue : number = 3.28;
 
   //Metods
   convertToFahrenheit() : void {
@@ -19,5 +21,13 @@ export class Converter {
 
   convertToCersius() : void {
     this.celsiusValue = parseFloat(((this.fahrenValue - 32 ) * 5/9).toFixed(2));
+  }
+
+  convertToFeet() : void {
+    this.feetValue = parseFloat((this.meterValue * 3.28).toFixed(1));
+  }
+
+  convertToMeter() : void {
+    this.meterValue = parseFloat((this.feetValue * 0.3048).toFixed(1));
   }
 }
