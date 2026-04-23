0b9eca7618f16934b5ff10b1cd91e62854cdea37
umavanmoriel <umeda19971102@gmail.com>
2026-04-23 17:48:35 +0200

    --Skapar input för temperatur konverter i converter.html --Lägger till class med metoder för att konvertera celsius till fahrenheit och vice versa i converter.ts -- Lägger till css för temperatur konverter

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule,],
   templateUrl: './converter.html',
   styleUrl: './converter.css',
 })
-export class Converter {}
+export class Converter {
+  // Properties
+  celsiusValue : number = 0;
+  fahrenValue : number = 32;
+
+  //Metods
+  convertToFahrenheit() : void {
+    this.fahrenValue = parseFloat(((this.celsiusValue * 1.8 ) + 32).toFixed(2));
+  }
+
+  convertToCersius() : void {
+    this.celsiusValue = parseFloat(((this.fahrenValue - 32 ) * 5/9).toFixed(2));
+  }
+}
